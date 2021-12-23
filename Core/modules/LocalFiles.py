96833c4275b4011baa6cef4dfcbcf88075661c49
Sandip Low <sandipbkt2002@gmail.com>
2021-12-23 09:12:38 +0530

    music player code updated...

--- Core/modules/LocalFiles.py
+++ Core/modules/LocalFiles.py
@@ -2,48 +2,83 @@ import os
 import webbrowser
 
 
-music_dir = 'C:\\Users\\ASUS\\Music'
+class Location_Information :
+    def __init__(self) :
+        self.music_dir = [os.environ['USERPROFILE'] + '\\Music']
+        self.songs = []
 
-songs = os.listdir(music_dir)
+        for x in self.music_dir :
+            self.songs.extend(os.listdir(x))
+
+    def getMusicDir(self) :
+        return self.music_dir
+
+    def setMusicDir(self, x) :
+        self.music_dir = x
+
+    def addMusicDir(self, x) :
+        self.music_dir.append(x)
+
+        for x in self.music_dir :
+            self.songs.extend(os.listdir(x))
+        
+
+li = Location_Information()
+
+songs = li.songs
+music_dir = li.music_dir[0]
+
+# i = 1
+# print("\n")
+# for x in songs:
+#     print(i, ":", x, "\n")
+#     i = i + 1
+
 
 def PlayMusic(query) :
 
     i = 0
 
-    if 'play mon bebagi' in query:
-        os.startfile(os.path.join(music_dir, 'Ley-Chakka-Kunal-Ganjawala_Prasenjit-Mukherjee.mp3'))
+    # if 'play mon bebagi' in query:
+    #     os.startfile(os.path.join(music_dir, 'Ley-Chakka-Kunal-Ganjawala_Prasenjit-Mukherjee.mp3'))
 
-    elif 'play char chakka' in query or 'play four six' in query:
-        os.startfile(os.path.join(music_dir, 'Char Chokka Hoi Hoi (ICC WT20 Cricket Theme Song).mp3'))
+    # elif 'play char chakka' in query or 'play four six' in query:
+    #     os.startfile(os.path.join(music_dir, 'Char Chokka Hoi Hoi (ICC WT20 Cricket Theme Song).mp3'))
 
-    elif 'play i am a rider' in query or 'play i\'m a rider' in query:
-        os.startfile(os.path.join(music_dir, 'I Am a Rider_192(PaglaSongs).mp3'))
+    # elif 'play i am a rider' in query or 'play i\'m a rider' in query:
+    #     os.startfile(os.path.join(music_dir, 'I Am a Rider_192(PaglaSongs).mp3'))
 
-    elif 'play kuch to bata zindagi' in query or 'play life tell something' in query:
-        os.startfile(os.path.join(music_dir, 'Zindagi Kuch Toh Bata.mp3'))
+    # elif 'play kuch to bata zindagi' in query or 'play life tell something' in query:
+    #     os.startfile(os.path.join(music_dir, 'Zindagi Kuch Toh Bata.mp3'))
 
-    elif 'play zindagi ek safar' in query or 'play life is a journey' in query:
-        os.startfile(os.path.join(music_dir, 'Zindagi Ek Safar Hai Suhana - Kishore Kumar (DJJOhAL.Com).mp3'))
+    # elif 'play zindagi ek safar' in query or 'play life is a journey' in query:
+    #     os.startfile(os.path.join(music_dir, 'Zindagi Ek Safar Hai Suhana - Kishore Kumar (DJJOhAL.Com).mp3'))
 
-    elif 'play cat vibing' in query:
+    # elif 'play g t a' in query : 
+    #     os.startfile(os.path.join(music_dir, 'Grand-Theft-Auto-San-Andreas-Theme-Song.mp3'))
+
+    if 'play cat vibing' in query:
         webbrowser.open("https://www.youtube.com/watch?v=NUYvbT6vTPs")
-
-    elif 'play g t a' in query : 
-        os.startfile(os.path.join(music_dir, 'Grand-Theft-Auto-San-Andreas-Theme-Song.mp3'))
 
     elif 'play music' in query:
         os.startfile(os.path.join(music_dir, songs[i]))
 
     elif 'play next music' in query : 
-        os.startfile(os.path.join(dir, os.listdir(dir)[i+1]))
+        os.startfile(os.path.join(dir, os.listdir(dir)[i]))
         i+=1
+
+    else :
+        query = query.replace('play ', '')
+
+        for s in songs:
+            if query in s.replace('-', ' ').lower() :
+                os.startfile(os.path.join(music_dir, s))
 
 
 def openProgram(query) :
 
     if 'open google drive' in query:
-        dir = "C:\\Users\\ASUS\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Brave Apps"
-        os.startfile(os.path.join(dir, os.listdir(dir)[0]))
+        webbrowser.open('https://drive.google.com')
 
     elif 'open powerpoint' in query:
         dir = 'C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Microsoft Office'
@@ -52,10 +87,4 @@ def openProgram(query) :
     elif 'open paint' in query :
         dir = 'C:\\Windows\\system32\\mspaint.exe'
         os.startfile(os.path.join(dir))
-
-i = 1
-print("\n")
-for x in songs:
-    print(i, ":", x, "\n")
-    i = i + 1
 
---
+++ README.md
@@ -0,0 +1,40 @@
+This is a python project that is able to Listen, Speak and do actions on the basis of your command
+
+First install the dependencies by using executing the command in Terminal by using : 
+
+    pip install Requirement.txt
+
+Then you have to install vosk api from here --> https://alphacephei.com/vosk/models/vosk-model-en-in-0.4.zip.
+Then extrct it in this directory.
+
+
+For working offline :
+    Uses vosk api. A bit unaccurate.
+
+1. open the Terminal in this directory and execute the command : 
+    
+        python init.py
+
+2. Enter your name : ___
+3. Enter 1 or 2 as per voice preference
+4. Enter command
+
+        start
+    
+5. Now it is ready to use....
+
+For working online :
+    Uses Speech_recognizer module. More accurate.
+
+1. Start offline mode first.
+2. Then say the command "start online mode"
+
+
+
+Use the following commands for test :
+
+    'what is the time now' --- know the time
+    'what is the day/date today' --- know the day/date of that day
+    'search ___ from google/wikipedia/youtube' -- search some thing from as you wish
+    'tell me about ___' -- listen about something from wikipedia.
+    'open youtube' -- open YT for you
